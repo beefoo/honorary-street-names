44570c51d8e09ac5dba442a138311ba890929edc
Brian Foo <brian@youaremyjoy.org>
2017-03-18 11:02:17 -0400

    update encoding

--- download_html.py
+++ download_html.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import os
-import urllib
+import urllib2
 
 URL_PATTERN = "http://nycstreets.info/honorStreet.asp?b=%s&letter=%s"
 BOROUGHS = ["BX","BK","M","Q","SI"]
 LETTERS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 OUTPUT_DIR = "html/"
+ENCODING = "ISO-8859-1"
+OVERWRITE = True
+
+def downloadUrl(url, filename):
+    print "Downloading %s" % url
+    req = urllib2.urlopen(url)
+    content = req.read()
+    ucontent = unicode(content, ENCODING)
+    with open(filename, 'w') as f:
+        f.write(ucontent.encode('utf-8'))
 
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
@@ -19,11 +29,8 @@ for b in BOROUGHS:
 
     for l in LETTERS:
         filename = boroughDir + l + ".html"
-
-        if not os.path.isfile(filename):
+        if not os.path.isfile(filename) or OVERWRITE:
             url = URL_PATTERN % (b, l)
-            print "Downloading %s" % url
-            f = urllib.URLopener()
-            f.retrieve(url, filename)
+            downloadUrl(url, filename)
 
 print "Done."
